6b7294a956c98bd12f899714aab195fc7a62a58b
klintat <teberga.klinta@gmail.com>
2022-09-26 21:11:24 +0300

    login

--- Project/Web_store_project/database/seeders/UserSeeder.php
+++ Project/Web_store_project/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name'=>'Jon Dove',
             'email'=>'jon.dove@gmail.com',
-            'password'=>'54321'
+            'password'=>Hash::make('54321')
         ]);
     }
 }
