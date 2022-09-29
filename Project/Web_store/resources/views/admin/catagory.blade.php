794cecbb2d3a75407f18c3b38c20c32c2fcb6459
klintat <teberga.klinta@gmail.com>
2022-09-29 11:37:52 +0300

    catagory table

--- Project/Web_store/resources/views/admin/catagory.blade.php
+++ Project/Web_store/resources/views/admin/catagory.blade.php
@@ -22,6 +22,16 @@
         {
             color: black;
         }
+
+        .center
+        {
+          margin: auto;
+          width: 50%;
+          text-align: center;
+          margin-top: 30px;
+          border: 3px solid white;
+        }
+
     </style>
   </head>
   <body>
@@ -33,13 +43,44 @@
         <!-- partial -->
         <div class="main-panel">
           <div class="content-wrapper">
+
+            @if(session()->has('message'))
+
+            <div class="alert alert-success">
+
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                
+                    {{session()->get('message')}}
+            </div>
+
+            @endif
                 <div class="div_center">
                    <h2 class="h2_font"> Add catagory </h2>
-                   <form>
-                    <input class="input_color" type="text" name="name" placeholder="Write catagory name">
+                   <form action="{{url('/add_catagory')}}" method="POST">
+                    @csrf
+                    <input class="input_color" type="text" name="catagory" placeholder="Write catagory name">
                     <input type="submit" class="btn btn-primary" name="submit" value="Add Catagory">
                     </form>
-                </div>  
+                </div>
+                
+                <table class="center">
+
+                  <tr>
+                    <td>Catagroty Name</td>
+                    <td>Action</td>
+                  </tr>
+                  @foreach($data as $data)
+
+                  <tr>
+                    <td>{{$data->catagory_name}}</td>
+                    <td>
+                      <a onclick="return confirm('Do you want to continue and delete this?')" class="btn btn-danger" href="{{url('delete_catagory',$data->id)}}">Delete</a>
+                    </td>
+                  </tr>
+
+                  @endforeach
+
+                </table>
           </div>
         </div>
     <!-- container-scroller -->
